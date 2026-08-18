3a5b8264bd9bbef728ef90e4e724052431fbc05a
Azka azeem <azkaazeem804@gmail.com>
2026-08-18 20:04:32 +0500

    starts local storage work

--- routes/auth.js
+++ routes/auth.js
@@ -31,14 +31,21 @@ router.post("/register", async (req, res) => {
 
 router.post("/login" , async (req , res) => {
     try {
-    const user = await User.findOne({email:req.body.email});
-    !user && res.status(400).json("Wrong credentials!");
-    const validPassword = await bcrypt.compare(req.body.password, user.password)
-    !validPassword && res.status(400).json("Wrong password!");
+        const user = await User.findOne({email:req.body.email});
 
-    res.status(200).json(user);
+        if (!user) {
+            return res.status(400).json("Wrong credentials!");
+        }
+
+        const validPassword = await bcrypt.compare(req.body.password, user.password);
+
+        if (!validPassword) {
+            return res.status(400).json("Wrong password!");
+        }
+
+        return res.status(200).json(user);
     } catch (err) {
-        res.status(500).json(err);
+        return res.status(500).json(err);
     }
 })
 
